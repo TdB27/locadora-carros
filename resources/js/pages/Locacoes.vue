b6ac8debd6197619c5d427b50f5e5b8162996276
Thiago Billy <thiagobilly27@gmail.com>
2023-02-03 15:45:17 -0300

    feat: inserção das funções remover e filtrar de locacoes

--- resources/js/pages/Locacoes.vue
+++ resources/js/pages/Locacoes.vue
@@ -73,7 +73,7 @@
                                     separator: ' ',
                                 },
                                 valor_diaria: {
-                                    titulo: 'Data Final Realizado',
+                                    titulo: 'Valor da Diária',
                                     tipo: 'amount',
                                 },
                                 km_inicial: {
@@ -429,7 +429,6 @@
                     />
                 </input-container-component>
             </template>
-            -->
         </modal-component>
         <!-- fim do Modal de visualização de locacoes -->
 
@@ -590,6 +589,229 @@
             </template>
         </modal-component>
         <!-- fim do Modal de atualização de locacoes -->
+
+        <!-- inicio do Modal de remover de locacoes -->
+        <modal-component
+            id="modalRemoverLocacoes"
+            :titulo="'Remover Locação ' + $store.state.item.id"
+        >
+            <template v-slot:alertas>
+                <alert-component
+                    tipo="success"
+                    titulo="Cadastro removido com sucesso"
+                    :detalhes="$store.state.transacao"
+                    v-if="$store.state.transacao.status == 'sucesso'"
+                ></alert-component>
+                <alert-component
+                    tipo="danger"
+                    titulo="Erro ao tentar remover a locação"
+                    :detalhes="$store.state.transacao"
+                    v-if="$store.state.transacao.status == 'erro'"
+                ></alert-component>
+            </template>
+
+            <template v-slot:conteudo>
+                <input-container-component
+                    titulo="Nome do Cliente"
+                    id="visualizarCliente"
+                    id-help=""
+                    texto-ajuda=""
+                >
+                    <input
+                        type="text"
+                        class="form-control"
+                        id="visualizarCliente"
+                        :value="$store.state.item.cliente?.nome"
+                        disabled
+                    />
+                </input-container-component>
+
+                <input-container-component
+                    titulo="Carro / Placa"
+                    id="visualizarCarro"
+                    id-help=""
+                    texto-ajuda=""
+                >
+                    <input
+                        type="text"
+                        class="form-control"
+                        id="visualizarCarro"
+                        :value="
+                            $store.state.item.carro?.modelo?.nome +
+                            ' / ' +
+                            $store.state.item.carro?.placa
+                        "
+                        disabled
+                    />
+                </input-container-component>
+
+                <input-container-component
+                    titulo="Data de Inicio"
+                    id="visualizarDataInicio"
+                    id-help=""
+                    texto-ajuda=""
+                >
+                    <input
+                        type="text"
+                        class="form-control"
+                        id="visualizarDataInicio"
+                        :value="
+                            $filters.formataDataTempo(
+                                $store.state.item.data_inicio_periodo,
+                                ' '
+                            )
+                        "
+                        disabled
+                    />
+                </input-container-component>
+            </template>
+            <template v-slot:rodape>
+                <button type="button" class="btn btn-danger" @click="remover()">
+                    Remover
+                </button>
+            </template>
+        </modal-component>
+        <!-- fim do Modal de remover de locacoes -->
+
+        <!-- inicio do Modal de filtro de locacoes -->
+        <modal-component id="modalFiltros" titulo="Filtrar Locações">
+            <template v-slot:conteudo>
+                <input-container-component
+                    titulo="Nome do Cliente"
+                    id="filtrarCliente"
+                    id-help="filtrarClienteHelp"
+                    texto-ajuda="Informe o Cliente"
+                >
+                    <select
+                        class="form-select"
+                        v-model="busca.cliente_id"
+                        aria-label="filtrarModelo"
+                    >
+                        <option v-for="c in clientes" :key="c.id" :value="c.id">
+                            {{ c.nome }}
+                        </option>
+                    </select>
+                </input-container-component>
+
+                <input-container-component
+                    titulo="Carro / Placa"
+                    id="filtrarCarro"
+                    id-help="filtrarCarroHelp"
+                    texto-ajuda="Informe o Carro desejado"
+                >
+                    <select
+                        class="form-select"
+                        v-model="busca.carro_id"
+                        aria-label="filtrarCarro"
+                    >
+                        <option v-for="c in carros" :key="c.id" :value="c.id">
+                            {{ c.modelo.nome }} - {{ c.placa }}
+                        </option>
+                    </select>
+                </input-container-component>
+
+                <input-container-component
+                    titulo="Data de Inicio"
+                    id="filtrarDataInicio"
+                    id-help="filtrarDataInicioHelp"
+                    texto-ajuda="Informe a Data de Inicio do Período"
+                >
+                    <input
+                        type="date"
+                        class="form-control"
+                        id="filtrarDataInicio"
+                        v-model="busca.data_inicio_periodo"
+                    />
+                </input-container-component>
+
+                <input-container-component
+                    titulo="Data Final Previsto"
+                    id="filtrarDataFinalPrevista"
+                    id-help="filtrarDataFinalPrevistaHelp"
+                    texto-ajuda="Informe a Data Final Prevista do Período"
+                >
+                    <input
+                        type="date"
+                        class="form-control"
+                        id="filtrarDataFinalPrevista"
+                        v-model="busca.data_final_previsto_periodo"
+                    />
+                </input-container-component>
+
+                <input-container-component
+                    titulo="Data Final Realizado"
+                    id="filtrarDataFinalRealizado"
+                    id-help="filtrarDataFinalRealizadoHelp"
+                    texto-ajuda="Informe a Data Final Realizado do Período"
+                >
+                    <input
+                        type="date"
+                        class="form-control"
+                        id="filtrarDataFinalRealizado"
+                        v-model="busca.data_final_realizado_periodo"
+                    />
+                </input-container-component>
+
+                <input-container-component
+                    titulo="Valor da Diária"
+                    id="filtrarValorDiaria"
+                    id-help="filtrarValorDiariaHelp"
+                    texto-ajuda="Informe o Valor da Diária"
+                >
+                    <input
+                        type="number"
+                        class="form-control"
+                        id="filtrarValorDiaria"
+                        aria-describedby="filtrarValorDiariaHelp"
+                        placeholder="Valor da Diária"
+                        v-model="busca.valor_diaria"
+                    />
+                </input-container-component>
+
+                <input-container-component
+                    titulo="Km Inicial"
+                    id="filtrarKmInicial"
+                    id-help="filtrarKmInicialHelp"
+                    texto-ajuda="Informe o Km Inicial"
+                >
+                    <input
+                        type="number"
+                        class="form-control"
+                        id="filtrarKmInicial"
+                        aria-describedby="filtrarKmInicialHelp"
+                        placeholder="Km Inicial"
+                        v-model="busca.km_inicial"
+                    />
+                </input-container-component>
+
+                <input-container-component
+                    titulo="Km Final"
+                    id="filtrarKmFinal"
+                    id-help="filtrarKmFinalHelp"
+                    texto-ajuda="Informe o Km Inicial"
+                >
+                    <input
+                        type="number"
+                        class="form-control"
+                        id="filtrarKmFinal"
+                        aria-describedby="filtrarKmFinalHelp"
+                        placeholder="Km Final"
+                        v-model="busca.km_final"
+                    />
+                </input-container-component>
+            </template>
+
+            <template v-slot:rodape>
+                <button
+                    type="button"
+                    class="btn btn-primary"
+                    @click="filtrar()"
+                >
+                    Filtrar
+                </button>
+            </template>
+        </modal-component>
+        <!-- fim do Modal de filtro de locacoes -->
     </div>
 </template>
 
@@ -682,6 +904,74 @@ export default {
                     this.$store.state.transacao.dados =
                         errors.response.data.errors;
                 });
+        },
+        remover() {
+            let url = this.urlBase + "/" + this.$store.state.item.id;
+
+            let form = {
+                _method: "delete",
+            };
+
+            axios
+                .post(url, form)
+                .then((response) => {
+                    this.$store.state.transacao.status = "sucesso";
+                    this.$store.state.transacao.mensagem =
+                        "Registro de cliente removido com sucesso!";
+                    this.carregarLista();
+                })
+                .catch((errors) => {
+                    this.$store.state.transacao.status = "erro";
+                    this.$store.state.transacao.mensagem =
+                        errors.response.data.erro;
+                });
+        },
+        filtrar() {
+            let filtro = "";
+
+            for (let chave in this.busca) {
+                let operator = "like";
+                let percentSign = "%";
+
+                if (this.busca[chave]) {
+                    if (filtro != "") filtro += ";";
+
+                    if (
+                        [
+                            "cliente_id",
+                            "carro_id",
+                            "valor_diaria",
+                            "km_inicial",
+                            "km_final",
+                        ].includes(chave)
+                    )
+                        percentSign = "";
+
+                    if (
+                        ["cliente_id", "carro_id", "valor_diaria"].includes(
+                            chave
+                        )
+                    )
+                        operator = "=";
+
+                    if (chave == "km_inicial") operator = ">=";
+                    if (chave == "km_final") operator = "<=";
+
+                    let operatorSign = ":" + operator + ":";
+
+                    filtro +=
+                        chave + operatorSign + this.busca[chave] + percentSign;
+                }
+            }
+
+            if (filtro != "") {
+                this.urlPaginacao = "page=1";
+                this.urlFiltro = "&filtro=" + filtro;
+            } else {
+                this.urlFiltro = "";
+            }
+
+            this.carregarLista();
         },
     },
     watch: {
